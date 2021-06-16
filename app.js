542317d01de0aaec9d52a85a11dc500dfb9c1028
ryma1738 <51891293+ryma1738@users.noreply.github.com>
2021-06-16 13:40:06 -0600

    suck me

--- app.js
+++ app.js
@@ -10,11 +10,130 @@ const iqr = require('inquirer');
 //     console.log('Portfolio complete! Check out index.html to see the output!');
 //   });
 
-iqr
-    .prompt([
+function promptUser() {
+    return iqr.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is your name?",
+                validate: nameInput => {
+                    if (nameInput) {
+                      return true;
+                    } else {
+                      console.log('Please enter your name!');
+                      return false;
+                    }
+                  }
+            },
+            {
+                type: 'input',
+                name: 'github',
+                message: 'Enter your GitHub Username:',
+                validate: nameInput => {
+                    if (nameInput) {
+                      return true;
+                    } else {
+                      console.log('Please enter your github username!');
+                      return false;
+                    }
+                  }
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAbout',
+                message: 'Would you like to enter some information about yourself for an "About" section?',
+                default: true
+            },
+            {
+                type: 'input',
+                name: 'about',
+                message: 'Provide some information about yourself:',
+                when: ({ confirmAbout }) => {
+                    if (confirmAbout) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        ])
+}
+
+function promptProject(portfolioData) {
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+    return iqr.prompt([
         {
             type: 'input',
             name: 'name',
-            message: "What is your name?"
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                  return true;
+                } else {
+                  console.log('Please enter the name of your project!');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (required):',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                  return true;
+                } else {
+                  console.log('Please enter a description for your project');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply):',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub Link to your Project. (Required):',
+            validate: link => {
+                if (link) {
+                  return true;
+                } else {
+                  console.log('Please enter your name!');
+                  return false;
+                }
+              }
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?'
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
         }
-    ]).then(answers => console.log(answers));
+    ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+          } else {
+            return portfolioData;
+          }
+      });
+}
+promptUser()
+.then(promptProject)
+.then(portfolioData => console.log(portfolioData));
